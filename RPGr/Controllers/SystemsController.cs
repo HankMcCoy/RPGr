cc24d50ccaa592cccc82020dc5f346e50a2aca20
Thomas Beirne <Thomas.Beirne@gmail.com>
2013-01-17 22:46:44 -0800

    Basic ASP.NET MVC 4 app
    It begins.

---
+++ RPGr/Controllers/SystemsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPGr.Controllers
+{
+    public class SystemsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
